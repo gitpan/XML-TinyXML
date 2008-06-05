fd6b17fd22302f80614b139259006cd5fea190e0
Andrea Guzzo <xant@cpan.org>
2008-06-05 12:35:30 +0000

    Import of XANT/XML-TinyXML-0.02 from CPAN.
    gitpan-cpan-distribution: XML-TinyXML
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         XANT/XML-TinyXML-0.02.tar.gz
    gitpan-cpan-author:       XANT
    gitpan-cpan-maturity:     released

--- lib/XML/TinyXML.pm
+++ lib/XML/TinyXML.pm
@@ -134,7 +134,7 @@ our @EXPORT = qw(
 	XmlSubstBranch
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
