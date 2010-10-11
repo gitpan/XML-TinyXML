fea79a30007b10f404a479fa64c4766f453cdf55
Andrea Guzzo <xant@cpan.org>
2010-10-11 10:56:09 +0000

    Import of XANT/XML-TinyXML-0.28 from CPAN.
    gitpan-cpan-distribution: XML-TinyXML
    gitpan-cpan-version:      0.28
    gitpan-cpan-path:         XANT/XML-TinyXML-0.28.tar.gz
    gitpan-cpan-author:       XANT
    gitpan-cpan-maturity:     released

--- lib/XML/TinyXML/Selector/XPath.pm
+++ lib/XML/TinyXML/Selector/XPath.pm
@@ -80,7 +80,7 @@ use XML::TinyXML::Selector::XPath::Context;
 use XML::TinyXML::Selector::XPath::Functions;
 use XML::TinyXML::Selector::XPath::Axes;
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 #our @ExprTokens = ('(', ')', '[', ']', '.', '..', '@', ',', '::');
 
